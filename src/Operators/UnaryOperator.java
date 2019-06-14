b3347d31ce6c119cc452dd9a9c182cadb69f1292
Rajesh <rajm6344@gmail.com>
2019-06-14 19:23:19 -0400

    operators example

--- src/Operators/UnaryOperator.java
+++ src/Operators/UnaryOperator.java
@@ -1,7 +1,16 @@
 package Operators;
 
+
 public class UnaryOperator {
     public static void main(String[] args) {
-        
+      int a = 1;
+      int b = 0;
+      int c = 1;
+      int d = 0;
+
+      b =  a++;
+      System.out.println("The value of a postfix is : "+ b);  // The value of a postfix is : 1
+      d = --c;
+      System.out.println("The value of a prefix is : "+ d);  //The value of a prefix is : 0
     }
 }
