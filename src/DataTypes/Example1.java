4be48ee980d746ac493e8a6f5ec1ceee072f4983
Rajesh <rajm6344@gmail.com>
2019-06-13 23:00:42 -0400

    my data type examples

--- src/DataTypes/Example1.java
+++ src/DataTypes/Example1.java
@@ -3,5 +3,14 @@ package DataTypes;
 public class Example1 {
     public static void main(String[] args) {
         System.out.println("myfirst program");
+
+        int a = 89;
+        System.out.println(" value of integer is "+a);
+
+        // a = 0 ; it allows
+        // a = -9; it allows
+        // a = 7.89; THis is not allowed in java. it will through error
+        // Error:(10, 13) java: incompatible types: possible lossy conversion from double to int
+        System.out.println(" value of integer is "+a);
     }
 }
---
+++ src/DataTypes/Example2.java
@@ -0,0 +1,8 @@
+package DataTypes;
+
+public class Example2 {
+    public static void main(String[] args) {
+        char b = 'B';
+        System.out.println("The value of char is :"+b );
+    }
+}
