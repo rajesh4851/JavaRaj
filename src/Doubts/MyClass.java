25499a7e3d72e150aa734bb49a94e96a1b46ef8f
Rajesh <rajm6344@gmail.com>
2019-06-28 21:12:09 -0400

    new excercises

---
+++ src/Doubts/MyClass.java
@@ -0,0 +1,11 @@
+package Doubts;
+
+public class MyClass {
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
