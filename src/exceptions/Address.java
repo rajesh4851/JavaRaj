48ce893b9667f97c2b6311efa628ef6f42199725
Rajesh <rajm6344@gmail.com>
2019-07-09 23:40:05 -0400

    exceptions

---
+++ src/exceptions/Address.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+ public class Address throws execption {
+    public String address1;
+    public String address2;
+    public String zip;
+    public String country;
+}
---
+++ src/exceptions/Person.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+// Plain Old Java Objects / Domain Objects / DTO Data transfer Objects
+public class Person {
+    public String name;
+    public int height;
+    public int weigth;
+
+    Address address;
+    String defaultVariable;
+}
